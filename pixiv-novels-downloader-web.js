55006fd4fac31686a9fd86a860218360c88a3ad5
Koki Takahashi <hakatasiloving@gmail.com>
2014-12-14 07:56:35 +0900

    Add conversion of pixiv tags

--- pixiv-novels-downloader-web.js
+++ pixiv-novels-downloader-web.js
@@ -79,6 +79,10 @@ app.post('/', function (req, res) {
 			// preprocess novel text
 
 			novel = novel.replace(/《/g, '≪').replace(/》/g, '≫');
+			novel = novel.replace(/､/g, '、').replace(/｡/g, '。');
+			novel = novel.replace(/｢/g, '「').replace(/｣/g, '」');
+			novel = novel.replace(/!/g, '！').replace(/\?/g, '？');
+			novel = novel.replace(/([！？]{2,})/g, '［＃縦中横］$1［＃縦中横終わり］');
 			novel = novel.replace(/\[chapter:(.+?)\]/g, '［＃中見出し］$1［＃中見出し終わり］');
 			novel = novel.replace(/\[newpage\]/g, '［＃改ページ］');
 
